45e48843611ddd1a5a31699a35a0ee248637dba8
rohitsm146 <rsmottomanavar@gmail.com>
2026-09-08 16:27:07 +0530

    JS Completed and Project Started

---
+++ Student_Task_Management_Portal/src/App.jsx
@@ -0,0 +1,13 @@
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <div>
+      <Welcome />
+      <Welcome />
+      <Welcome />
+    </div>
+  );
+}
+
+export default App;
---
+++ Student_Task_Management_Portal/src/components/Welcome.jsx
@@ -0,0 +1,7 @@
+function Welcome(){
+    return (
+        <h1>Welcome to Student Task Management Portal</h1>
+    );
+}
+
+export default Welcome;
